9c7e8022f2e3ed5d9dfc121813fb97b37fc28c9f
choijangjun <jangjun9644@gmail.com>
2023-02-14 14:42:33 +0900

    명령어 입력

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,20 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String cmd = sc.nextLine();
+		while(true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();
+			
+			System.out.println("입력된 명령어 : " + cmd);
+			
+			if(cmd.equals("exit")) {
+				break;
+			}
+			
+			
+		}
 		
-		
-		System.out.println("입력된 명령어 : " + cmd);
-		
-		System.out.println("== 프로그램 시작==");
+		System.out.println("== 프로그램 끝==");
 		
 		sc.close();
 		
